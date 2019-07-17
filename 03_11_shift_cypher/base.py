076978b719af840f923d24154076ee1c7dcfa2ff
gibsonbailey <gibson.bailey@live.com>
2019-07-17 12:36:08 -0700

    Added chagrade dummy sub file for chagrade development testing.

--- 03_11_shift_cypher/base.py
+++ 03_11_shift_cypher/base.py
@@ -1,10 +1,18 @@
-def encode(plaintext, shift):
-    return 'string'
+import time
 
+
+def encode(plaintext, shift):
+    b = bytearray(plaintext, 'utf-8')
+    for i in range(len(plaintext)):
+        c = b[i]
+        if c > 64 and c < 91:
+            b[i] = (c - 65 + shift) % 26 + 65
+        elif c > 96 and c < 123:
+            b[i] = (c - 97 + shift) % 26 + 97
+    return b.decode()
 
 def decode(cypher_text, shift):
-    return 'string'
-
+    return encode(cypher_text, -shift)
 
 # Basic Test
 assert encode('Identity theft is not a joke, Jim!', 5) == "Nijsynyd ymjky nx sty f otpj, Onr!"
@@ -21,3 +29,16 @@ print('Decode Tests Passed')
 # This import statement can go a bit wonky? Make sure your file is in the submission folder when running this
 # from tests import full_test
 # full_test(encode, decode)
+
+start = time.time()
+# Basic Test
+encode('Identity theft is not a joke, Jim!', 5) 
+encode('Bears, beets, Battlestar Galactica', 30)
+encode('I want people to be afraid of how much they love me.', 8)
+
+decode('Z bevn vortkcp nyrk kf uf. Slk ze r dlty dfiv ivrc jvejv, Z yru ef zuvr nyrk kf uf.', 17)
+decode("L grq'w kdwh lw. L mxvw grq'w olnh lw dw doo dqg lw'v whuuleoh", 3)
+decode('Ufm gq Hsqrgac Zcytcp?', 24)
+#print('Decode Tests Passed')
+end = time.time()
+print('time:', end - start)
